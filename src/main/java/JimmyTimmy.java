55c2b312ae6735fe785cf46bfa76223082b3d7d2
Chewy03 <156920786+Chewy03@users.noreply.github.com>
2025-08-21 01:08:19 +0800

    Level-1

--- src/main/java/JimmyTimmy.java
+++ src/main/java/JimmyTimmy.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class JimmyTimmy {
     public static void main(String[] args) {
         String lb = "____________________________________________________________\n";
         String greet = lb + "Hello! I'm JimmyTimmy\n" + "What can I do for you?\n" + lb;
         String logoff = "Bye. Hope to see you again soon!\n" + lb;
-        System.out.println(greet + logoff);
+        System.out.println(greet);
+
+        Scanner scanner = new Scanner(System.in);
+        String echo = "";
+        while (true) {
+            echo = scanner.nextLine();
+            if (echo.equalsIgnoreCase("bye")) {
+                System.out.println(lb + "\n" + logoff);
+                break;
+            } else {
+                System.out.println(lb + echo + "\n" + lb);
+            }
+        }
+        scanner.close();
     }
 }
