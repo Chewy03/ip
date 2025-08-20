90aa7f7c22415818e8b612bc883ecf56187cfdba
Chewy03 <156920786+Chewy03@users.noreply.github.com>
2025-08-21 00:54:01 +0800

    Level-0

--- src/main/java/JimmyTimmy.java
+++ src/main/java/JimmyTimmy.java
@@ -4,6 +4,5 @@ public class JimmyTimmy {
         String greet = lb + "Hello! I'm JimmyTimmy\n" + "What can I do for you?\n" + lb;
         String logoff = "Bye. Hope to see you again soon!\n" + lb;
         System.out.println(greet + logoff);
-//        System.out.println(logoff);
     }
 }
